6fb1b96b860dba5b6e7c18b31b16b6f1b7bfb94d
TechPanelGM <giomook2003@gmail.com>
2025-09-18 23:18:09 +0200

    lemonadep: Add Oplus camera

--- lineage_lemonadep.mk
+++ lineage_lemonadep.mk
@@ -15,6 +15,9 @@ $(call inherit-product, device/oneplus/lemonadep/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+
 PRODUCT_NAME := lineage_lemonadep
 PRODUCT_DEVICE := lemonadep
 PRODUCT_MANUFACTURER := OnePlus
